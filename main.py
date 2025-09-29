a188d99285f75b1ccff49fe922deb51d1e44c2ef
choieunjung082 <lovecej131@gmail.com>
2025-09-29 16:20:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# 제목
+st.title("🌍 MBTI 국가별 데이터 미리보기")
+
+# 데이터 불러오기
+df = pd.read_csv("countriesMBTI_16types.csv")
+
+# 상위 5줄 표시
+st.subheader("데이터 상위 5줄")
+st.dataframe(df.head())
 
